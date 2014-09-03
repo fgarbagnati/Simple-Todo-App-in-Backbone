c6365828bf81fb48c15ad80e77f662795fbcbbeb
fgarbagnati <f.g.garbagnati@gmail.com>
2014-09-03 11:26:42 -0700

    fixing little thannngs

--- js/collections/todos.js
+++ js/collections/todos.js
@@ -2,7 +2,7 @@ var app = app || {};
 
 var TodoList = Backbone.Collection.extend({
   model: app.Todo,
-  localStorage: new Backbone.LocalStorate('todos-backbone'),
+  localStorage: new Backbone.LocalStorage('todos-backbone'),
   completed: function() {
     return this.filter(function( todo ) {
       return todo.get('completed');
